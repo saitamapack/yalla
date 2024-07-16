bf4f932dbcec19e731af04cc3041472a87b229f5
saitamapack <97965030+saitamapack@users.noreply.github.com>
2024-07-16 09:46:11 +0000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ function get_data($url) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true); // Enable SSL verification
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // Updated value for SSL verification
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true); // Enable SSL verification
     curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
